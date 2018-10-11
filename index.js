e70072179c023aa1959b876277d5a594ac66e5e2
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-10 23:38:45 -0400

    Added Middleware
    #COMMENT
    
    Plugged global middleware to the server

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const server = express();
 
+const logger = (req, res, next) => {
+// next will be called in every middleware we build
+// It determines when to go to the next piece of middleware
+console.log(`${Date.now()} ${req.method} made to ${req.url}`);
+next();
+};
+
+//Plugs global middleware into our server
+server.use(logger);
+
 server.get('/api', (req, res) => {
     res.send('Hello-World!');
 });
