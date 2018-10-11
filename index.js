4caf6cd2ff1d79e4092ef67f4636ec80022eb8ce
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-10 22:12:31 -0400

    Added Request Handler
    #COMMENT
    
    Added server.get() method

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const server = express();
 
-const port = 7000;
-server.listen(port, () => console.log(`API is running on ${port}`));
-
 server.get('/api', (req, res) => {
     res.send('Hello-World!');
 });
+
+const port = 7000;
+server.listen(port, () => console.log(`API is running on ${port}`));
+
