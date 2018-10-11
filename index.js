cf28081ee6a3576d7edbd4d15143411701bf33b2
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-11 15:07:05 -0400

    Added Yell Middleware & Route
    #COMMENT
    
    Added middleware to manipulate name to uppercase
    Passed it to route and rendered into client

--- index.js
+++ index.js
@@ -14,10 +14,16 @@ const logger = (req, res, next) => {
 };
 
 const greeter = (req, res, next) => {
-    req.section = 'FSW-!3';
+    req.section = 'FSW-13';
     next();
 };
 
+const yell = (req, res, next) => {
+    const newName = req.params.name.toUpperCase();
+    req.name = newName;
+    next();
+}
+ 
 
 
 //Plugs in logger which is a global middleware into our server
@@ -25,6 +31,11 @@ server.use(logger);
 // built a GET endpoint to `/` that returns the string hello world to the client.
 
 // ROUTES
+
+server.get('/section', greeter, (req, res) => {
+    res.send(`Hello ${req.section}, NVM`);
+});
+
 server.get('/', (req, res) => {
   res.send('Cowabunga!');
 });
@@ -32,6 +43,11 @@ server.get('/', (req, res) => {
 server.get('/rapha', (req, res) => {
   res.send('I am Raphael');
 });
+
+server.get('/name/:name', yell, (req, res) => {
+    res.send(req.name);
+});
+
 // call server.listen w/ a port of your choosing
 server.listen(port, () => {
   console.log(`Booyahkasha happening on ${port}`);
