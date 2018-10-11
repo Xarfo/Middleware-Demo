d071a5f6d05567d6f88ac55370e2680a64f2dfbf
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-10 23:52:22 -0400

    Added Order
    #COMMENT
    
    Order: middleware, routes and then server.listen()

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const server = express();
 
+//Call middlewares always before routes and server.listen()
 const logger = (req, res, next) => {
 // next will be called in every middleware we build
 // It determines when to go to the next piece of middleware
@@ -9,13 +10,15 @@ console.log(`${Date.now()} ${req.method} made to ${req.url}`);
 next();
 };
 
-//Plugs global middleware into our server
+//Plugs global(in existence across our app) middleware into our server
 server.use(logger);
 
+// Routes
 server.get('/api', (req, res) => {
     res.send('Hello-World!');
 });
 
+//Call server.listen() method that creates listner @ port 7000
 const port = 7000;
 server.listen(port, () => console.log(`API is running on ${port}`));
 
